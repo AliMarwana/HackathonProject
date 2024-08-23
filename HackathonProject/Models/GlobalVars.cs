34e73a054b6dbc6afdabebef7bd2a92c7e6cc92f
abel <abel>
2024-08-23 15:42:18 +0100

    Variables Globals

--- HackathonProject/Models/GlobalVars.cs
+++ HackathonProject/Models/GlobalVars.cs
@@ -2,11 +2,122 @@
 {
     public class GlobalVars
     {
+        public GlobalVars()
+        {
+            
+        }
         public bool IsAutomaticMode { get; set; }
         public bool IsManualMode { get; set; }
         public bool ControlVoltageOff { get; set; }
         public bool ControlVoltageOn { get; set; }
         public bool IsLac01On { get; set; }
         public bool IsLac02On { get; set; }
+
+        public bool Controller_1 { get; set; }
+        public bool moteur_1 { get; set; }
+        public bool capter_1 { get; set; }
+
+        public bool Controller_2 { get; set; }
+        public bool moteur_2 { get; set; }
+        public bool capter_2 { get; set; }
+
+        public bool Controller_3 { get; set; }
+        public bool moteur_3 { get; set; }
+        public bool capter_3 { get; set; }
+
+        public bool Controller_4_1 { get; set; }
+        public bool moteur_4_1 { get; set; }
+        public bool capter_4_1 { get; set; }
+
+        public bool Controller_4_2 { get; set;}
+        public bool moteur_4_2 { get; set;}
+        public bool capter_4_2 { get; set;}
+
+
+        public bool Controller_5 { get; set;}
+        public bool moteur_5 { get; set;}
+        public bool capter_5 { get; set;}
+        public bool capter_5_Enter_Close { get; set;}
+
+        public bool Controller_6_1 { get; set; }
+        public bool moteur_6_1 { get; set; }
+        public bool capter_6_1 { get; set; }
+
+        public bool Controller_6_2 { get; set;}
+        public bool moteur_6_2 { get; set;}
+        public bool capter_6_2 { get; set;}
+
+        public bool Controller_6_3 { get; set; }
+        public bool moteur_6_3 { get; set; }
+        public bool capter_6_3 { get; set; }
+
+        public bool Controller_7 { get; set; }
+        public bool moteur_7 { get; set; }
+        public bool capter_7 { get; set; }
+
+        public bool Controller_8 { get; set; }
+        public bool moteur_8 { get; set; }
+        public bool capter_8 { get; set; }
+
+        public bool Controller_9 { get; set; }
+        public bool moteur_9 { get; set; }
+        public bool capter_9 { get; set; }
+
+        public bool Controller_10 { get; set; }
+        public bool moteur_10 { get; set; }
+        public bool capter_10 { get; set; }
+
+        public bool Controller_11 { get; set; }
+        public bool moteur_11 { get; set; }
+        public bool capter_11 { get; set; }
+
+        public bool Controller_12 { get; set; }
+        public bool moteur_12 { get; set; }
+        public bool capter_12_Bottom { get; set; }
+        public bool capter_12_Top { get; set; }
+        public bool capter_12_NoOverTemperature { get; set; }
+
+        public bool Controller_13_1 { get; set; }
+        public bool moteur_13_1 { get; set; }
+        public bool capter_13_1 { get; set; }
+
+        public bool Controller_13_2 { get; set; }
+        public bool moteur_13_2 { get; set; }
+        public bool capter_13_2 { get; set; }
+
+        public bool Controller_14 { get; set; }
+        public bool moteur_14 { get; set; }
+        public bool capter_14 { get; set; }
+
+        public bool Controller_15 { get; set; }
+        public bool moteur_15 { get; set; }
+        public bool capter_15 { get; set; }
+
+        public bool Controller_16 { get; set; }
+        public bool moteur_16 { get; set; }
+        public bool capter_16_Bottom { get; set; }
+        public bool capter_16_Top { get; set; }
+        public bool capter_16_NoOverTemperature { get; set; }
+
+        //Lac02 Controllers
+        //Lac Start Or Off
+        public bool StartOrOffLac2 { get; set; }
+
+        public bool Controller_2_2 { get; set; }
+        public bool moteur_2_2 { get; set; }
+        public bool capter_2_2 { get; set; }
+        public bool capter_2_2_BackSide { get; set; }
+
+        public bool Controller_3_1 { get; set; }
+        public bool moteur_3_1 { get; set; }
+        public bool capter_3_1 { get; set; }
+
+        public bool Controller_3_2 { get; set; }
+        public bool moteur_3_2 { get; set; }
+        public bool capter_3_2 { get; set; }
+
+        public bool Controller_4 { get; set; }
+        public bool moteur_4 { get; set; }
+        public bool capter_4 { get; set; }
     }
 }
--- HackathonProject/Services/GVariablesService.cs
+++ HackathonProject/Services/GVariablesService.cs
@@ -11,12 +11,6 @@ namespace HackathonProject.NewFolder
         {
             _adsClient = client;    
         }
-        public bool ControlVoltageOn { get; set; }
-        public bool ControlVoltageOff { get; set; }
-        public bool IsLac1On { get; set; }
-        public bool IsLac2On { get; set; }
-        public bool IsManual { get; set; }
-        public bool IsAutomatic { get; set; }
         
         public T ReadVar<T>(string nameVar)
         {
@@ -33,12 +27,123 @@ namespace HackathonProject.NewFolder
         } 
         public GlobalVars GetGlobal()
         {
-            _adsClient.Connect(AmsNetId.Local, 851);
             bool controlVoltageOff = ReadVar<bool>("GIO.IbIP02_ControlVoltageOff_NC");
             bool controlVoltageOn = ReadVar<bool>("GIO.IbIP02_ControlVoltageOn");
             GlobalVars globalVars = new GlobalVars();
-            globalVars.ControlVoltageOff = controlVoltageOff;
-            globalVars.ControlVoltageOn = controlVoltageOn;
+            globalVars.ControlVoltageOff = ReadVar<bool>("GIO.IbIP02_ControlVoltageOff_NC");
+            globalVars.ControlVoltageOn = ReadVar<bool>("GIO.IbIP02_ControlVoltageOn");
+
+            globalVars.Controller_1 = ReadVar<bool>("GIO.Ib0101_ARC_ControllerReady");
+            globalVars.moteur_1 = ReadVar<bool>("GIO.Ob0101_ARC_EnableController");
+            globalVars.capter_1 = ReadVar<bool>("GIO.Ib0101_ARC_Stop");
+
+
+            globalVars.Controller_2 = ReadVar<bool>("GIO.Ib0102_ARC_ControllerReady");
+            globalVars.moteur_2 = ReadVar<bool>("GIO.Ob0102_ARC_EnableController");
+            globalVars.capter_2 = ReadVar<bool>("GIO.Ib0102_ARC_GapDetection_1_RC0204");
+
+            globalVars.Controller_3 = ReadVar<bool>("GIO.Ib0103_ARC_ControllerReady");
+            globalVars.moteur_3 = ReadVar<bool>("GIO.Ob0103_ARC_EnableController");
+            globalVars.capter_3 = ReadVar<bool>("GIO.Ib0103_ARC_Stop");
+
+            globalVars.Controller_4_1 = ReadVar<bool>("GIO.Ib0104e01_RC_ControllerReady");
+            globalVars.moteur_4_1 = ReadVar<bool>("GIO.Ob0104e01_RC_EnableController");
+            globalVars.capter_4_1 = ReadVar<bool>("GIO.Ib0104e01_RC_Stop");
+
+            globalVars.Controller_4_2 = ReadVar<bool>("GIO.Ib0104e02_RC_ControllerReady");
+            globalVars.moteur_4_2 = ReadVar<bool>("GIO.Ob0104e02_RC_EnableController");
+            globalVars.capter_4_2 = ReadVar<bool>("GIO.Ib0104e02_RC_Stop");
+
+            globalVars.Controller_5 = ReadVar<bool>("GIO.Ib0105_ARC_ControllerReady");
+            globalVars.moteur_5 = ReadVar<bool>("GIO.Ob0105_ARC_EnableController");
+            globalVars.capter_5 = ReadVar<bool>("GIO.Ib0105_ARC_Stop");
+            globalVars.capter_5_Enter_Close = ReadVar<bool>("GIO.Ib0105_ARC_Closed");
+
+            globalVars.Controller_6_1 = ReadVar<bool>("GIO.Ib0106e01_ARC_ControllerReady");
+            globalVars.moteur_6_1 = ReadVar<bool>("GIO.Ob0106e01_ARC_EnableController");
+            globalVars.capter_6_1 = ReadVar<bool>("GIO.Ib0106e01_ARC_Stop");
+
+            globalVars.Controller_6_2 = ReadVar<bool>("GIO.Ib0106e02_RC_ControllerReady");
+            globalVars.moteur_6_2 = ReadVar<bool>("GIO.Ob0106e02_RC_EnableController");
+            globalVars.capter_6_2 = ReadVar<bool>("GIO.Ib0106e02_RC_Stop");
+
+            globalVars.Controller_6_3 = ReadVar<bool>("GIO.Ib0106e03_RC_ControllerReady");
+            globalVars.moteur_6_3 = ReadVar<bool>("GIO.Ob0106e03_RC_EnableController");
+            globalVars.capter_6_3 = ReadVar<bool>("GIO.Ib0106e03_RC_Stop");
+
+            globalVars.Controller_7 = ReadVar<bool>("GIO.Ib0107_ARC_ControllerReady");
+            globalVars.moteur_7 = ReadVar<bool>("GIO.Ob0107_ARC_EnableController");
+            globalVars.capter_7 = ReadVar<bool>("GIO.Ib0107_ARC_Stop");
+
+            globalVars.Controller_8 = ReadVar<bool>("GIO.Ib0108_ARC_ControllerReady");
+            globalVars.moteur_8 = ReadVar<bool>("GIO.Ob0108_ARC_EnableController");
+            globalVars.capter_8 = ReadVar<bool>("GIO.Ib0108_ARC_Stop");
+
+            globalVars.Controller_9 = ReadVar<bool>("GIO.Ib0109_ARC_ControllerReady");
+            globalVars.moteur_9 = ReadVar<bool>("GIO.Ob0109_ARC_EnableController");
+            globalVars.capter_9 = ReadVar<bool>("GIO.Ib0109_ARC_Stop");
+
+            globalVars.Controller_10 = ReadVar<bool>("GIO.Ib0110_ARC_ControllerReady");
+            globalVars.moteur_10 = ReadVar<bool>("GIO.Ob0110_ARC_EnableController");
+            globalVars.capter_10 = ReadVar<bool>("GIO.Ib0110_ARC_Stop");
+
+            globalVars.Controller_11 = ReadVar<bool>("GIO.Ib0111_ARC_ControllerReady");
+            globalVars.moteur_11 = ReadVar<bool>("GIO.Ob0111_ARC_EnableController");
+            globalVars.capter_11 = ReadVar<bool>("GIO.Ib0111_ARC_Stop");
+
+            globalVars.Controller_12 = ReadVar<bool>("GIO.Ib0112_SLC_ControllerReady");
+            globalVars.moteur_12 = ReadVar<bool>("GIO.Ob0109_ARC_EnableController");
+            globalVars.capter_12_Bottom = ReadVar<bool>("GIO.Ib0112_LT_Bottom");
+            globalVars.capter_12_Top = ReadVar<bool>("GIO.Ib0112_LT_Top");
+            globalVars.capter_12_NoOverTemperature = ReadVar<bool>("GIO.Ib0112_SLC_NoOverTemperatureMotor");
+
+
+            globalVars.Controller_13_1 = ReadVar<bool>("GIO.Ib0113e01_ARC_ControllerReady");
+            globalVars.moteur_13_1 = ReadVar<bool>("GIO.Ob0113e01_ARC_EnableController");
+            globalVars.capter_13_1 = ReadVar<bool>("GIO.Ib0113e01_ARC_Stop");
+
+            globalVars.Controller_13_2 = ReadVar<bool>("GIO.Ib0113e02_ARC_ControllerReady");
+            globalVars.moteur_13_2 = ReadVar<bool>("GIO.Ob0113e02_ARC_EnableController");
+            globalVars.capter_13_2 = ReadVar<bool>("GIO.Ib0113e02_ARC_Stop");
+
+            globalVars.Controller_14 = ReadVar<bool>("GIO.Ib0114_ARC_ControllerReady");
+            globalVars.moteur_14 = ReadVar<bool>("GIO.Ob0114_ARC_EnableController");
+            globalVars.capter_14 = ReadVar<bool>("GIO.Ib0114_ARC_Stop");
+
+            //After
+
+            globalVars.Controller_15 = ReadVar<bool>("GIO.Ib0115_ARC_ControllerReady");
+            globalVars.moteur_15 = ReadVar<bool>("GIO.Ob0115_ARC_EnableController");
+            globalVars.capter_15 = ReadVar<bool>("GIO.Ib0115_ARC_Stop");
+
+            globalVars.Controller_16 = ReadVar<bool>("GIO.Ib0116_SLC_ControllerReady");
+            globalVars.moteur_16 = ReadVar<bool>("GIO.Ob0116_SLC_EnableController");
+            globalVars.capter_16_Bottom = ReadVar<bool>("GIO.Ib0116_LT_Bottom");
+            globalVars.capter_16_Top = ReadVar<bool>("GIO.Ib0116_LT_Top");
+            globalVars.capter_16_NoOverTemperature = ReadVar<bool>("GIO.Ib0116_SLC_NoOverTemperatureMotor");
+
+            //Lac02 Controllers
+            //Lac Start Or Off
+            globalVars.StartOrOffLac2 = ReadVar<bool>("GIO.Ib02_LAC_EnergyIsolationSwitchOn");
+
+
+            globalVars.Controller_2_2 = ReadVar<bool>("GIO.Ib0202_RC_ControllerReady");
+            globalVars.moteur_2_2 = ReadVar<bool>("GIO.Ob0202_RC_EnableController");
+            globalVars.capter_2_2 = ReadVar<bool>("GIO.Ib0202_RC_Stop");
+            globalVars.capter_2_2_BackSide = ReadVar<bool>("GIO.Ib0202_RC_BackSide");
+
+            globalVars.Controller_3_1 = ReadVar<bool>("GIO.Ib0203e01_RC_ControllerReady");
+            globalVars.moteur_3_1 = ReadVar<bool>("GIO.Ob0203e01_RC_EnableController");
+            globalVars.capter_3_1 = ReadVar<bool>("GIO.Ib0203e01_RC_Stop");
+
+
+            globalVars.Controller_3_2 = ReadVar<bool>("GIO.Ib0203e02_RC_ControllerReady");
+            globalVars.moteur_3_2 = ReadVar<bool>("GIO.Ob0203e02_RC_EnableController");
+            globalVars.capter_3_2 = ReadVar<bool>("GIO.Ib0203e02_RC_Stop");
+
+            globalVars.Controller_4 = ReadVar<bool>("GIO.Ib0204_RC_ControllerReady");
+            globalVars.moteur_4 = ReadVar<bool>("GIO.Ob0204_RC_EnableController");
+            globalVars.capter_4 = ReadVar<bool>("GIO.Ib0204_RC_Stop");
 
             return globalVars;
         }
